7cce86573c3ea8fc4fecbce215f2f5024ba6f3c4
leancah7 <schvartzbergleandro23@gmail.com>
2025-03-17 17:00:35 -0300

    mi primer commit

---
+++ tarea2.js
@@ -0,0 +1,14 @@
+function eje2(){
+
+var valores = [true, 5, false, "hola", "adios", 2];
+   
+ if (valores[4].length > valores[3].length)
+ {
+    console.log ("el valor "+valores[4]+ "es mayor que "+valores[3]);
+    console.log (valores[0])
+ }
+ else{
+    console.log ("el valor "+valores[3]+ " es mayor que "+valores[4]);
+    console.log (valores[2])
+}   
+}
