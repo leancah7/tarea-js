c3eba2df2f0719478455add23df9eecc8b39ca5a
leancah7 <schvartzbergleandro23@gmail.com>
2025-03-31 15:24:21 -0300

    10 ejercicios

---
+++ 1/main.js
@@ -0,0 +1,17 @@
+function imprimirMeses() {
+    let array = [];
+    array.push("Enero");
+    array.push("Febrero");
+    array.push("Marzo");
+    array.push("Abril");
+    array.push("Mayo");
+    array.push("Junio");
+    array.push("Julio");
+    array.push("Agosto");
+    array.push("Septiembre");
+    array.push("Octubre");
+    array.push("Noviembre");
+    array.push("Diciembre");
+    for (let i = 0; i < 12; i++)
+        console.log(array[i]);
+}
